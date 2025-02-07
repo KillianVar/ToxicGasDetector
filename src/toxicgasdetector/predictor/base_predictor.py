713477cd58eb09ab26f7c2c59c4ea37cbfe300e9
KillianVar <varesconkillian@gmail.com>
2025-02-07 13:12:54 +0100

    Added in job summary infos about the ML module used.

--- src/toxicgasdetector/predictor/base_predictor.py
+++ src/toxicgasdetector/predictor/base_predictor.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import torch
 import json
+import pickle
 
 
 class Pipeline:
@@ -30,13 +31,17 @@ class Pipeline:
             'Model Bases': model_bases,
             'Data Filepath': str(self.data_filepath),
             'Output Directory': str(self.output_dir),}
-        if "sklearn" in model_bases:
+        if pd.Series(model_bases).str.contains('sklearn').any():
             #TODO : for sklearn, save parameters and pickle the model.
-            pass
+            job_info['Python ML Module'] = 'sklearn'
+            with open(self.output_dir / 'model_trained.pkl', 'wb') as f:
+                pickle.dump(self.model, f)
+            
         
-        elif pd.Series(model_bases).str.contains('torch').any() :
+        elif pd.Series(model_bases).str.contains('torch').any():
             #TODO : for torch models, save only the model with the torch method.
             torch.save(self.model, self.output_dir / "model_trained.pt")
+            job_info['Python ML Module'] = 'torch'
 
         with open(self.output_dir / 'job_info.json', 'w') as f:
             json.dump(job_info, f)
